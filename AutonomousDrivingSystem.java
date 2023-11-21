72e80813b9834955b0c864967a447765b2c16305
Abhi Patel <apatel477@myseneca.ca>
2023-11-20 23:48:25 -0500

    Added AutonomousDrivingSystem

--- AutonomousDrivingSystem.java
+++ AutonomousDrivingSystem.java
@@ -2,4 +2,23 @@
 class AutonomousDrivingSystem {
     // write a function to initialize data
     // write a function to run the overall simulation
+
+    private ControlModule controlModule;
+
+    public AutonomousDrivingSystem() {
+        this.controlModule = new ControlModule();
+    }
+
+    void initGpsData(double latitude1, double longitude1, double latitude2, double longitude2) {
+        controlModule.initializeGpsData(latitude1, longitude1, latitude2, longitude2);
+    }
+
+    void runSimulation() {
+        for (int hour = 0; hour < 24; hour++) {
+            // Simulate once per hour
+            controlModule.adjustSpeedAndDirection(new PerceptionModule(), new PlanningModule());
+            // Increment time in the PerceptionModule
+            new PerceptionModule().incrementTime();
+        }
+    }
 }
