80200c6161dc0df0ba2ebe5f70aba437297ffee4
Abhi Patel <apatel477@myseneca.ca>
2023-11-20 23:47:51 -0500

    Added PlanningModule...need tofix alot

--- PlanningModule.java
+++ PlanningModule.java
@@ -2,4 +2,40 @@
 class PlanningModule {
     // write a function to plan your route based on current and destination gps data
     // write a function to update your route based on camera and lidar data
+
+        // Route information
+        private double[] plannedRoute; // Array of waypoints
+        private int currentWaypointIndex;
+    
+        PlanningModule() {
+            // Initialize route planning data (simplified for demonstration)
+            plannedRoute = new double[]{32.0, 32.0}; // Destination waypoint
+            currentWaypointIndex = 0;
+        }
+    
+        // Function to plan the initial route based on current and destination GPS data
+        void planRoute(double currLatitude, double currLongitude, double destLatitude, double destLongitude) {
+            // Simplified route planning: Direct line from current to destination
+            plannedRoute = new double[]{destLatitude, destLongitude};
+            currentWaypointIndex = 0;
+    
+            System.out.println("Route planned: Direct line from current to destination");
+        }
+    
+        // Function to update the route based on camera and LIDAR data
+        void updateRoute(CameraData cameraData, LidarData lidarData) {
+            // Simplified route updating based on camera and LIDAR data (no updates in this example)
+            System.out.println("Route updated: No updates based on camera and LIDAR data");
+        }
+    
+        // Function to get the next waypoint in the route
+        double[] getNextWaypoint() {
+            if (currentWaypointIndex < plannedRoute.length) {
+                double[] nextWaypoint = {plannedRoute[currentWaypointIndex], plannedRoute[currentWaypointIndex + 1]};
+                currentWaypointIndex += 2;
+                return nextWaypoint;
+            } else {
+                return null;
+            }
+        }
 }
