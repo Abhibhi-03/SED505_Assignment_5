c998ea80602ef2af55fd3a812accad329d169e6b
Abhi Patel <apatel477@myseneca.ca>
2023-11-20 23:13:31 -0500

    Added AutonomousDriving class

---
+++ AutonomousDrivingSystem.java
@@ -0,0 +1,5 @@
+// Autonomous Driving System - starts, runs and stops the simulation
+class AutonomousDrivingSystem {
+    // write a function to initialize data
+    // write a function to run the overall simulation
+}
