f7c496e512f2445acc669b1990b3b0dfe1be3411
Abhi Patel <apatel477@myseneca.ca>
2023-11-20 23:12:28 -0500

    Added ControlModule Class

---
+++ ControlModule.java
@@ -0,0 +1,9 @@
+// Control Module - this module brings together gps data, sensor data, and planning data
+class ControlModule {
+    // write a function to initialize GPS data
+    // write a function to adjust speed and direction based on:
+    // - the processing of sensor data
+    // - the planning of your route
+    // - the updating of your route
+    // end your journey after 24 hours or within 25km of your destination
+}
