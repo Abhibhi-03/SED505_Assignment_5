c0c7dd73826ee927c1bb5df19135417eb1e833fb
Abhi Patel <apatel477@myseneca.ca>
2023-11-20 23:11:47 -0500

    Added PlanningModule Class

---
+++ PlanningModule.java
@@ -0,0 +1,5 @@
+// Planning Module - plans a route based on GPS data and obstacle information (LIDAR data)
+class PlanningModule {
+    // write a function to plan your route based on current and destination gps data
+    // write a function to update your route based on camera and lidar data
+}
